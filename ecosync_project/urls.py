0f477d5e66001137b41452006982d570378ab80e
SHADIK <shadik.sk420@gmail.com>
2024-05-04 19:12:04 +0600

    redirected to fronend removed

--- ecosync_project/urls.py
+++ ecosync_project/urls.py
@@ -7,7 +7,7 @@ admin.site.site_header = 'EcoSync Administration'
 admin.site.index_title = 'Navigation'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='http://127.0.0.1:5173/dashboard')),
+    # path('', RedirectView.as_view(url='http://127.0.0.1:5173/dashboard')),
     path('auth/', include('ecosync.urls')),
     path('', include('user_management.urls')),
     path('', include('profile_management.urls')),
